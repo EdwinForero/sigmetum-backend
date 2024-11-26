949452a74d35d7f3e5189bd5b9f4ca84eff4e86e
jupbc <jupbcan100@hotmail.com>
2024-11-26 16:29:59 -0500

    change: excel column mapping

--- functions/convertExcelToJson.js
+++ functions/convertExcelToJson.js
@@ -6,35 +6,32 @@ const convertExcelToJson = (filePath) => {
     const worksheet = workbook.Sheets[firstSheetName];
     const jsonData = XLSX.utils.sheet_to_json(worksheet);
 
-    const columnMapping = {
-        "Provincia": "Provincia",
-        "Municipio": "Municipio",
-        "Alt. Media": "Altitud Media",
-        "Sector Biog.": "Sector Biogeográfico",
-        "Piso Biocl.": "Piso Bioclimático",
-        "Ombrot.": "Ombrotipo",
-        "Nat. Sustr.": "Naturaleza del Sustrato",
-        "Tipo Serie": "Tipo de Serie",
-        "Serie Veget.": "Serie de Vegetación",
-        "Veg. Pot.": "Vegetación Potencial",
-        "Esp. Recom.": "Especies Características",
-        "Esp. Características.": "Especies Características",
-    };
+    const columnMapping = [
+        "Provincia",
+        "Municipio",
+        "Altitud Media",
+        "Sector Biogeográfico",
+        "Piso Bioclimático",
+        "Ombrotipo",
+        "Naturaleza del Sustrato",
+        "Tipo de Serie",
+        "Serie de Vegetación",
+        "Vegetación Potencial",
+        "Especies Características"
+    ];
 
     const processedData = jsonData.map(row => {
         const processedRow = {};
-    
-        for (const [key, value] of Object.entries(row)) {
-            const formattedKey = columnMapping[key] || key;
-            if (!formattedKey.startsWith('__EMPTY') && value !== null && value !== '') {
+        Object.values(row).forEach((value, index) => {
+            const formattedKey = columnMapping[index];
+            if (formattedKey && value !== null && value !== '') {
                 if (typeof value === 'string' && value.includes(',')) {
                     processedRow[formattedKey] = value.split(',').map(item => item.trim()).filter(item => item !== '');
                 } else {
                     processedRow[formattedKey] = value;
                 }
             }
-        }
-    
+        });
         return processedRow;
     });
 
